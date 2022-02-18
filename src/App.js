9dc75afbb1e1fdc9e10d300122913f62191d51f0
Jéssica Florentino <jdgflorentino@gmail.com>
2022-02-18 15:21:28 -0300

    Requisito 2.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <div><Header /></div>
+      <div>
+        <Header />
+        <SolarSystem />
+      </div>
     );
   }
 }
